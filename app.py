f49bcd20999656c467ac65d023d349ae06d734ad
Jeff <vdr_jdiala@airasia.com>
2019-07-30 07:43:16 +0800

    save template for requests

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def root_func():
-    return "Test"
+    return "Hello World"
 
+
+@app.route("/from/headers")
+def get_from_headers():
+    pass
+
+@app.route("/from/query")
+def get_from_query_params():
+    pass
+
+@app.route("/from/url/<in_value>")
+def get_from_url(in_value):
+    pass
+
+@app.route("/from/body")
+def get_from_json():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
