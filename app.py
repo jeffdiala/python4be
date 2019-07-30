212e83577b37b43cd2a35a6eb1dc521a9943e183
Jeff <vdr_jdiala@airasia.com>
2019-07-30 08:28:48 +0800

    for Flask response

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def root_func():
+    # Returning a string will give status 200
     return "Test"
+
+    # try returning a different Status
+    # try returning a tuple
+    # try returning a list
+    # try returning a dictionary
 
 
 if __name__ == '__main__':
