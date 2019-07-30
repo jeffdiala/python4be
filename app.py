9ae7c4323b89c013f12f3bc1ab18c95b3b47741c
Jeff <vdr_jdiala@airasia.com>
2019-07-30 10:54:08 +0800

    solution for requests

--- app.py
+++ app.py
@@ -9,19 +9,29 @@ def root_func():
 
 @app.route("/from/headers")
 def get_from_headers():
-    pass
+    token = request.headers.get("token")
+
+    return token
+
 
 @app.route("/from/query")
 def get_from_query_params():
-    pass
+    lname = request.args.get("lname")
+    fname = request.args.get("fname")
+
+    return fname + " " + lname
+
 
 @app.route("/from/url/<in_value>")
 def get_from_url(in_value):
-    pass
+    return in_value
 
 @app.route("/from/body")
 def get_from_json():
-    pass
+    data = request.get_json()
+
+    return str(data["fname"]) + " " + str(data["lname"])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
