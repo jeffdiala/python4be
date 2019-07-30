b45a16ebe023d405622b5638fee6c1af87cf9aaa
Jeff <vdr_jdiala@airasia.com>
2019-07-30 08:11:19 +0800

    sample for redirect

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def root_func():
-    return "Test"
+    return redirect("https://www.mapua.edu.ph/")
 
 
 if __name__ == '__main__':
